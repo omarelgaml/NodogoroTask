b5209c7e73de9477acf9e3474340a27e2d79be2a
omarelgaml <omarelgaml97@gmail.com>
2020-03-30 01:19:10 +0200

    second stable version

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+import Header from './Header';
+import Landing from './Landing';
+
+const dashboard = () => <h2>dashboard</h2>;
+class App extends Component {
+  componentDidMount () {
+    this.props.fetchUser ();
+  }
+  render () {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={dashboard} />
+
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect (null, actions) (App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+class Header extends Component {
+  renderContect () {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login with google</a></li>;
+
+      default:
+        return [
+
+          <li key="2"><a href="/api/logout">Logout</a></li>,
+        ];
+    }
+  }
+  render () {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <Link
+              to={this.props.auth ? '/dashboard' : '/'}
+              className="left-brand-logo"
+            >
+              Emaily
+            </Link>
+            <ul className="right">
+              {this.renderContect ()}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+function mapStateToProps (state) {
+  return {auth: state.auth};
+}
+
+export default connect (mapStateToProps) (Header);
